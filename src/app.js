93a101e774c84b02ba9c9e82e9ac9f3db3345a08
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2020-08-31 12:37:57 -0300

    Add project

---
+++ src/app.js
@@ -0,0 +1,11 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(require('./routes'));
+
+module.exports = app;
